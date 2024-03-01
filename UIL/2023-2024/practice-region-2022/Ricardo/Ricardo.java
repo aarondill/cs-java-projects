2ecb6f2b7270f02c06a0b02865b8f1da48e44422
aarondill <aaronsacks2006@gmail.com>
2024-03-01 16:29:09 -0600

    UIL: region-practice continued

--- UIL/2023-2024/practice-region-2022/Ricardo/Ricardo.java
+++ UIL/2023-2024/practice-region-2022/Ricardo/Ricardo.java
@@ -6,6 +6,8 @@ public class Ricardo {
       int dataCount = dataScanner.nextInt();
       dataScanner.nextLine();
       for (int i = 0; i < dataCount; i++) {
+        String line = dataScanner.nextLine();
+        System.out.printf("Case #%d: %.0f\n", i + 1, Math.pow(2, line.chars().filter(c -> c == '|').count()));
         // Do Some Stuff
       }
     }
