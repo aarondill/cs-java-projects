434322bc50687042a51bdf6d19a97c65e81d0e96
aarondill <aaronsacks2006@gmail.com>
2024-03-03 03:57:00 -0600

    Template(UIL): Improve UIL starting template4

--- UIL/template/{{ $DIRNAME }}.java
+++ UIL/template/{{ $DIRNAME }}.java
@@ -1,15 +1,27 @@
 :DELIMS tag="{{ }}" stmt="#@" comment="%#"
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class {{ $BASENAME }} {
-  public static void main(String... args) {
-    try (Scanner dataScanner = new Scanner({{ $BASENAME }}.class.getResourceAsStream("./{{ ${BASENAME,} }}.dat"))) {
-      int dataCount = dataScanner.nextInt();
-      dataScanner.nextLine();
-      for (int i = 0; i < dataCount; i++) {
-        // Do Some Stuff
-      }
-    }
+  @SuppressWarnings("unused")
+  private static int caseNum = 1;
+  private static final String INPUT_FILE = "{{ ${BASENAME,} }}.dat";
 
+  private static void each(Scanner scan) {
+    // Parse the input:
+    scan.nextLine();
+    // Handle output:
+  }
+
+  public static void main(String... args) throws FileNotFoundException {
+    try (Scanner scan = new Scanner(new File(INPUT_FILE))) {
+      int dataCount = Integer.parseInt(scan.nextLine(), 10);
+      for (int i = 0; i < dataCount; i++, caseNum++)
+        each(scan);
+    } catch (FileNotFoundException e) {
+      System.err.println("Could not find file: " + INPUT_FILE);
+      throw e;
+    }
   }
 }
