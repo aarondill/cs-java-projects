ef151330676ae7e1e3caf15362555d00e3a1f45b
aarondill <aaronsacks2006@gmail.com>
2024-03-20 06:40:01 -0500

    fix: Lession_8.1/TwoDArrays

--- CSA/Lesson_8.1/TwoDArrays/TwoDArrays.java
+++ CSA/Lesson_8.1/TwoDArrays/TwoDArrays.java
@@ -13,32 +13,30 @@
  */
 public class TwoDArrays {
   public static void main(String... args) {
-    int[][] nums = new int[3][4];
-    nums[0][0] = 3;
-    nums[0][1] = 4;
-    nums[0][2] = 2;
-    nums[0][3] = 7;
-    nums[1][0] = 13;
-    nums[1][1] = 357;
-    nums[1][2] = 93;
-    nums[1][3] = 43;
-    nums[2][0] = 66;
-    nums[2][1] = 773;
-    nums[2][2] = 83;
-    nums[2][3] = 39;
+    String[][] classRoom = {{"Bob", "Carol", "Ted", "Alice", "Jose"}, {"Mary", "Seema", "Marcus", "Liam", "Veronica"},
+        {"Sue", "Billy", "Hannah", "Aaron", "Lauren"}};
+    classRoom[2][3] = "LILY";
+    classRoom[0][0] = "AMY";
+    classRoom[1][2] = "DAVID";
 
-    System.out.println(nums[0][0]);
-    System.out.println(nums[0][1]);
-    System.out.println(nums[0][2]);
-    System.out.println(nums[0][3]);
-    System.out.println(nums[1][0]);
-    System.out.println(nums[1][1]);
-    System.out.println(nums[1][2]);
-    System.out.println(nums[1][3]);
-    System.out.println(nums[2][0]);
-    System.out.println(nums[2][1]);
-    System.out.println(nums[2][2]);
-    System.out.println(nums[2][3]);
+    System.out.println(classRoom[0][0]);
+    System.out.println(classRoom[1][0]);
+    System.out.println(classRoom[2][0]);
 
+    System.out.println(classRoom[0][1]);
+    System.out.println(classRoom[1][1]);
+    System.out.println(classRoom[2][1]);
+
+    System.out.println(classRoom[0][2]);
+    System.out.println(classRoom[1][2]);
+    System.out.println(classRoom[2][2]);
+
+    System.out.println(classRoom[0][3]);
+    System.out.println(classRoom[1][3]);
+    System.out.println(classRoom[2][3]);
+
+    System.out.println(classRoom[0][4]);
+    System.out.println(classRoom[1][4]);
+    System.out.println(classRoom[2][4]);
   }
 }
