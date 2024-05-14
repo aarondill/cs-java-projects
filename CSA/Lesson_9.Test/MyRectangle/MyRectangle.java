d94fda836679731832a87b800bc8601e12d5bfae
aarondill <aaronsacks2006@gmail.com>
2024-05-14 09:58:09 -0500

    fix(CSA): Lesson_9.Test (part 2)

--- CSA/Lesson_9.Test/MyRectangle/MyRectangle.java
+++ CSA/Lesson_9.Test/MyRectangle/MyRectangle.java
@@ -1,4 +1,7 @@
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.geom.AffineTransform;
 
 /**
  * Primary java class: MyRectangle
@@ -19,7 +22,7 @@ public class MyRectangle extends Rectangle {
   private boolean border;
 
   public MyRectangle() {
-    super();
+    super(1, 1);
   }
 
   public MyRectangle(int x, int y, int width, int height, String color, String borderColor, boolean hasBorder) {
@@ -61,14 +64,14 @@ public class MyRectangle extends Rectangle {
 
   // Rotate clockwise the rectangle the number of degrees stored in the parameter degrees using the upper left corner (x,y) as an anchor. Precondition - degrees will always be 90, 180 or 270. If not, no action is taken.
   public void rotate(int degrees) {
-    switch (degrees) {
-      case 90 -> {
-      }
-      case 180 -> {
-      }
-      case 270 -> {
-      }
-    }
+    if (degrees != 90 && degrees != 180 && degrees != 270) return;
+    AffineTransform tfx = new AffineTransform();
+    tfx.rotate(Math.toRadians(degrees), this.x, this.y);
+    Rectangle bounds = tfx.createTransformedShape(this).getBounds();
+    this.x = bounds.x;
+    this.y = bounds.y;
+    this.width = bounds.width;
+    this.height = bounds.height;
   }
 
   public int area() {
@@ -77,6 +80,28 @@ public class MyRectangle extends Rectangle {
 
   public int perimeter() {
     return 2 * (this.width + this.height);
+  }
+
+  @Override
+  public Object clone() {
+    return super.clone();
+  }
+
+  public MyRectangle copy() {
+    return (MyRectangle) this.clone();
+  }
+
+  // Returns a String representation of the location, dimensions and colors of this rectangle. If there is no border, the border color is not printed.
+  @Override
+  public String toString() {
+    List<String> list = new ArrayList<>();
+    list.add("x = " + this.x);
+    list.add("y = " + this.y);
+    list.add("width = " + this.width);
+    list.add("height = " + this.height);
+    list.add("color = " + this.color);
+    if (this.border) list.add("borderColor = " + this.borderColor);
+    return String.join(" ", list);
   }
 
 }
