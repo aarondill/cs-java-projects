f079bf559530d58ae368695e78294639b5c40d9b
Aaron Dill <aaronsacks2006@gmail.com>
2024-09-25 08:28:29 -0500

    feat(UIL): DataStructures Set

--- UIL/2024-2025/DataStructures/DataStructures.java
+++ UIL/2024-2025/DataStructures/DataStructures.java
@@ -1,12 +1,25 @@
 /* A demo on various data structures */
 
 import structs.DynamicArray;
+import structs.Set.*;
 
 public class DataStructures {
   public static void main(String... args) {
-    DynamicArray<String> da = new DynamicArray<String>();
+    DynamicArray<String> da = new DynamicArray<>();
     da.add("Hello");
     da.add("World");
     System.out.println(da);
+
+    SetInterface<String> s = new Set<>();
+    s.add("Hello");
+    s.add("World");
+    s.add("Hello");
+    System.out.println(s);
+
+    SetInterface<String> es = new EfficientSet<>();
+    es.add("Hello");
+    es.add("World");
+    es.add("Hello");
+    System.out.println(es);
   }
 }
---
+++ UIL/2024-2025/DataStructures/structs/Set/Set.java
@@ -0,0 +1,43 @@
+package structs.Set;
+
+import java.util.ArrayList;
+
+/**
+ * This is an easier to understand Set, sacrificing performance to use ArrayList
+ */
+public class Set<T> implements SetInterface<T> {
+  ArrayList<T> data = new ArrayList<>();
+
+  /* NOTE: This is O(n) instead of O(1) */
+  public boolean add(T o) {
+    if (has(o)) return false;
+    return data.add(o);
+  }
+
+  /* NOTE: This is O(n) instead of O(1) */
+  public boolean has(T o) {
+    return data.contains(o);
+  }
+
+  public int size() {
+    return data.size();
+  }
+
+  public boolean remove(Object o) {
+    // we don't need removeAll, since we check before inserting that only one can be contained
+    return data.remove(o);
+  }
+
+  public void clear() {
+    data.clear();
+  }
+
+  public Object[] toArray() {
+    return data.toArray();
+  }
+
+  public String toString() {
+    return data.toString();
+  }
+
+}
