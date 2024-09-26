ef02d8818a94035e9396ce0735ff0603a96b6755
Aaron Dill <aaronsacks2006@gmail.com>
2024-09-25 23:59:37 -0500

    chore(DataStructures): initial commit

---
+++ UIL/2024-2025/DataStructures/app/src/test/java/DataStructuresTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataStructuresTest {
+  @Test
+  void test() {
+    System.out.println("NOTE: There is no test for this app yet!");
+    assertTrue(true);
+  }
+}
