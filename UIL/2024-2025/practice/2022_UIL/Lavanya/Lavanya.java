0240dc5f778e0fac5dca371710ea0763a2c0dc5b
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-05 00:20:30 -0500

    feat(UIL): Lavanya

--- UIL/2024-2025/practice/2022_UIL/Lavanya/Lavanya.java
+++ UIL/2024-2025/practice/2022_UIL/Lavanya/Lavanya.java
@@ -1,16 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Lavanya {
   @SuppressWarnings("unused")
   private static int caseNum = 1;
   private static final String INPUT_FILE = "lavanya.dat";
 
+  private static List<Integer> primeFactors(int n) {
+    List<Integer> pfs = new LinkedList<>(); // faster add
+    int tmp = n;
+    int sqrt = (int) Math.sqrt(n);
+    for (int i = 2; i < sqrt; i++) {
+      if (tmp % i != 0) continue;
+      pfs.add(i);
+      tmp /= i;
+      i--;
+    }
+    if (tmp > 1) pfs.add(tmp);
+    return pfs;
+  }
+
   private static void each(Scanner scan) {
     // Parse the input:
-    scan.nextLine();
+    int n = Integer.parseInt(scan.nextLine());
     // Handle output:
+    var pfs = primeFactors(n);
+    System.out.println(n + " = " + pfs.stream().map(String::valueOf).collect(Collectors.joining(" * ")));
   }
 
   public static void main(String... args) throws FileNotFoundException {
