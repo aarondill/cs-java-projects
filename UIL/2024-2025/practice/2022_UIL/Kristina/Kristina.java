2cc23a74424f8fac493232b0471bdeeb99401f37
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-07 14:16:33 -0500

    refactor: cleanup Kristina

--- UIL/2024-2025/practice/2022_UIL/Kristina/Kristina.java
+++ UIL/2024-2025/practice/2022_UIL/Kristina/Kristina.java
@@ -59,34 +59,34 @@ public class Kristina {
   private static int caseNum = 1;
   private static final String INPUT_FILE = "kristina.dat";
 
-  public static int pre(List<String> s) {
-    Stack<String> list = new Stack<>();
-    Stack<Integer> saved = new Stack<>();
-    list.addAll(s);
-    while (!list.isEmpty()) {
-      String val = list.pop();
-      if (Operator.parseable(val)) {
-        saved.add(Operator.fromSymbol(val).calculate(saved.pop(), saved.pop()));
-      } else {
-        saved.add(Integer.parseInt(val));
+  public static int pre(List<String> list) {
+    Stack<Integer> s = new Stack<>();
+    // NOTE: reverse the list
+    for (String val : list.reversed()) {
+      if (!Operator.parseable(val)) {
+        s.push(Integer.parseInt(val));
+        continue;
       }
+      int a = s.pop(), b = s.pop();
+      s.push(Operator.fromSymbol(val).calculate(a, b));
     }
-
-    return saved.pop();
+    if (s.size() != 1) throw new IllegalArgumentException("Invalid input: stack size is " + s.size());
+    return s.pop();
   }
 
   private static int post(List<String> list) {
-    Stack<String> s = new Stack<>();
-    for (String word : list) {
-      s.push(word);
-      if (!Operator.parseable(word)) continue; // this is an opperand
-
-      Operator op = Operator.fromSymbol(s.pop());
-      Integer b = Integer.parseInt(s.pop()), a = Integer.parseInt(s.pop());
-      s.push(String.valueOf(op.calculate(a, b))); // add the calculated value to the stack
+    Stack<Integer> s = new Stack<>();
+    for (String val : list) {
+      if (!Operator.parseable(val)) {
+        s.push(Integer.parseInt(val));
+        continue;
+      }
+      // NOTE: opposite order of pre
+      int b = s.pop(), a = s.pop();
+      s.push(Operator.fromSymbol(val).calculate(a, b));
     }
     if (s.size() != 1) throw new IllegalArgumentException("Invalid input: stack size is " + s.size());
-    return Integer.parseInt(s.pop());
+    return s.pop();
   }
 
   private static void each(Scanner scan) {
