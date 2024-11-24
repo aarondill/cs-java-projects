ba82deea0209abff3233931d1073abbec245a106
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 14:48:24 -0600

    feat: scrolls

--- UIL/2024-2025/clements/Ancient Scrolls/Solution.java
+++ UIL/2024-2025/clements/Ancient Scrolls/Solution.java
@@ -1,22 +1,33 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Solution {
-  @SuppressWarnings("unused")
-  private static int caseNum = 1;
-
-  private static void each(Scanner scan) {
-    // Parse the input:
-    scan.nextLine();
-    // Handle output:
+  private static boolean valid(char[] data) {
+    Deque<Character> stack = new ArrayDeque<>(); // Currently "open" characters
+    for (char c : data) {
+      if (Character.isLowerCase(c)) {
+        stack.push(c); // Open the character
+      } else {
+        if (stack.isEmpty()) return false; // We've found a close, but no open
+        char top = stack.pop();
+        // We've found a close, but it doesn't match the open
+        if (top != Character.toLowerCase(c)) return false;
+      }
+    }
+    // We've found a close for every open
+    return stack.isEmpty();
   }
 
-  public static void main(String... args) throws FileNotFoundException {
+  private static void each(Scanner scan) {
+    char[] data = scan.nextLine().toCharArray();
+    System.out.println(valid(data) ? "YES" : "NO");
+  }
+
+  public static void main(String... args) {
     try (Scanner scan = new Scanner(System.in)) {
       int dataCount = Integer.parseInt(scan.nextLine(), 10);
-      for (int i = 0; i < dataCount; i++, caseNum++)
+      for (int i = 0; i < dataCount; i++)
         each(scan);
     }
   }
