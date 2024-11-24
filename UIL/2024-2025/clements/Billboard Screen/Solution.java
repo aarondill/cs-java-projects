686c68ce0b4b09f436e562ca525fc9baa3b48fff
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 14:57:08 -0600

    feat: giveup billboard

--- UIL/2024-2025/clements/Billboard Screen/Solution.java
+++ UIL/2024-2025/clements/Billboard Screen/Solution.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 
 public class Solution {
 
+  private static String centered(String title, int w, int h) {
+    if (title.length() > w * h) return null;
+    throw new UnsupportedOperationException("This is hard.");
+  }
+
   private static void each(Scanner scan) {
-    // Parse the input:
+    int w = scan.nextInt();
+    int h = scan.nextInt();
     scan.nextLine();
-    // Handle output:
+    String title = scan.nextLine();
+    String content = centered(title, w, h);
+    if (content == null) System.out.println("Title not compatible with size.");
+    System.out.println("#".repeat(w + 2));
+    for (String line : content.split("\n"))
+      System.out.println("# " + line + " #");
+    System.out.println("#".repeat(w + 2));
   }
 
   public static void main(String... args) {
