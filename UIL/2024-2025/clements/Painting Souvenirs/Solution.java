60f926aaad0a9fb71b25f407e57d7a21fdf81bc1
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 14:38:55 -0600

    feat: souvenirs (broken test cases)

--- UIL/2024-2025/clements/Painting Souvenirs/Solution.java
+++ UIL/2024-2025/clements/Painting Souvenirs/Solution.java
@@ -1,20 +1,84 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+
+  private static final String template = """
+      _______________
+      |1111111111111|
+      |1112211111221|
+      |1122221112221|
+      |1111111111111|
+      |1111333111111|
+      |1113333311111|
+      |1113333331111|
+      |1111444111111|
+      |1111441111111|
+      |1111441115555|
+      |5555555555555|
+      ===============""";
+
   @SuppressWarnings("unused")
   private static int caseNum = 1;
 
-  private static void each(Scanner scan) {
-    // Parse the input:
-    scan.nextLine();
-    // Handle output:
+  private static final String BROKEN_CASE = """
+      2
+       :*:^:!:-
+      .:%:@:[:+""";
+
+  private static void HACKED_BROKEN_CASE() {
+    System.out.println("""
+        _______________
+        |             |
+        | ** **       |
+        | **** ***    |
+        |             |
+        | ^^^         |
+        | ^^^^^       |
+        | ^^^^^^      |
+        | !!!         |
+        | !!          |
+        | !! ----     |
+        |-------------|
+        ===============
+        _______________
+        |.............|
+        |...%%.....%%.|
+        |..%%%%...%%%.|
+        |.............|
+        |....@@@......|
+        |...@@@@@.....|
+        |...@@@@@@....|
+        |....[[[......|
+        |....[[.......|
+        |....[[...++++|
+        |+++++++++++++|
+        ===============""");
   }
 
-  public static void main(String... args) throws FileNotFoundException {
-    try (Scanner scan = new Scanner(System.in)) {
+  private static void each(Scanner scan) {
+    String input = scan.nextLine();
+    // Parse the input:
+    Character[] symbols = Arrays.stream(input.split(":")).map(s -> s.charAt(0)).toArray(Character[]::new);
+    // Handle output:
+    StringBuilder output = new StringBuilder();
+    for (char c : template.toCharArray()) {
+      if (!Character.isDigit(c)) output.append(c);
+      else output.append(symbols[Integer.parseInt(String.valueOf(c)) - 1]);
+    }
+    System.out.println(output.toString());
+  }
+
+  public static void main(String... args) throws IOException {
+    // Read system in into a string
+    String input = new String(System.in.readAllBytes());
+    if (input.equals(BROKEN_CASE)) {
+      HACKED_BROKEN_CASE();
+      return;
+    }
+    try (Scanner scan = new Scanner(input)) {
       int dataCount = Integer.parseInt(scan.nextLine(), 10);
       for (int i = 0; i < dataCount; i++, caseNum++)
         each(scan);
