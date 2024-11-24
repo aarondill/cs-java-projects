c23c9592430e23fe447aa84f3bfeb266262d9288
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 13:35:26 -0600

    feat: giveup lorena

--- UIL/2024-2025/clements/Lorena's Party/Solution.java
+++ UIL/2024-2025/clements/Lorena's Party/Solution.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
@@ -9,22 +9,36 @@ public class Solution {
 
   private static void each(Scanner scan) {
     // Parse the input:
-    int friendCount = scan.nextInt();
+    /* int friendCount = */ scan.nextInt();
     int limos = scan.nextInt();
     int capacity = scan.nextInt();
     scan.nextLine();
-    int[] friendArrivals = Arrays.stream(scan.nextLine().split()).mapToInt(Integer::parseInt).toArray();
+    List<Integer> friendArrivals = (Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt)
+        .collect(ArrayList::new, ArrayList::add, ArrayList::addAll));
+    friendArrivals.sort(Integer::compareTo);
     // Handle output:
+    // totalTime / capacity / limos = the amount of time for each limo
+    int maxWaitTime = 0;
+    int limoIndex = 0;
+    for (int i = 0; i < limos; i++) {
+      var start = limoIndex;
+      var end = start + Math.min(friendArrivals.size() / limos, capacity);
+      end = Math.min(end, friendArrivals.size());
+      limoIndex = end + 1;
+      if (start >= friendArrivals.size()) throw new IllegalStateException("Empty limo. We've failed?");
+      var limo = friendArrivals.subList(start, end);
+      if (limo.isEmpty()) continue;
+      int waitTime = limo.get(limo.size() - 1) - limo.get(0);
+      maxWaitTime = Math.max(maxWaitTime, waitTime);
+    }
+    System.out.println(maxWaitTime);
   }
 
-  public static void main(String... args) throws FileNotFoundException {
+  public static void main(String... args) {
     try (Scanner scan = new Scanner(System.in)) {
       int dataCount = Integer.parseInt(scan.nextLine(), 10);
       for (int i = 0; i < dataCount; i++, caseNum++)
         each(scan);
-    } catch (FileNotFoundException e) {
-      System.err.println("Could not find file: " + INPUT_FILE);
-      throw e;
     }
   }
 }
