b47e7f8695bfd3becaaec5150bc89b6298ba118f
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 15:58:51 -0600

    feat: parkour

--- UIL/2024-2025/clements/Parkour Civilization/Solution.java
+++ UIL/2024-2025/clements/Parkour Civilization/Solution.java
@@ -1,20 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Solution {
-  @SuppressWarnings("unused")
-  private static int caseNum = 1;
 
   private static void each(Scanner scan) {
     // Parse the input:
+    /* int n = */ scan.nextInt();
+    int k = scan.nextInt();
     scan.nextLine();
-    // Handle output:
+    List<Integer> nums = Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+    List<Integer> distances = new ArrayList<>(nums.size() * (int) Math.log(nums.size()));
+    for (int i = 0; i < nums.size(); i++)
+      for (int j = i; j < nums.size(); j++) {
+        if (i == j) continue;
+        distances.add(Math.abs(nums.get(i) - nums.get(j)));
+      }
+    distances.sort(null);
+    System.out.println(distances.get(k - 1));
   }
 
   public static void main(String... args) {
     try (Scanner scan = new Scanner(System.in)) {
-      int dataCount = Integer.parseInt(scan.nextLine(), 10);
-      for (int i = 0; i < dataCount; i++, caseNum++)
-        each(scan);
+      each(scan);
     }
   }
 }
