0157136a11fa56b2ea6f4f247905e3a41f3d619c
Aaron Dill <aaronsacks2006@gmail.com>
2024-11-24 15:09:41 -0600

    feat: ordering

--- UIL/2024-2025/clements/Ordering 2/Solution.java
+++ UIL/2024-2025/clements/Ordering 2/Solution.java
@@ -1,19 +1,43 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-  @SuppressWarnings("unused")
-  private static int caseNum = 1;
+
+  private static void swap(Object[] x, int a, int b) {
+    Object t = x[a];
+    x[a] = x[b];
+    x[b] = t;
+  }
 
   private static void each(Scanner scan) {
-    // Parse the input:
-    scan.nextLine();
-    // Handle output:
+    String[] data = scan.nextLine().split(" ");
+    int ops = 0;
+    // selection sort the array, counting the number of swaps
+    for (int i = 0; i < data.length; i++) {
+      int min = i;
+      for (int j = i + 1; j < data.length; j++) {
+        if (data[min].compareTo(data[j]) > 0) {
+          min = j;
+        }
+      }
+      if (min != i) {
+        swap(data, i, min);
+        ops++;
+      }
+    }
+
+    String[] sorted = Arrays.copyOf(data, data.length);
+    Arrays.sort(data);
+    if (!Arrays.equals(sorted, data)) {
+      throw new RuntimeException("unsorted: " + Arrays.toString(sorted));
+    }
+    System.out.println(ops);
   }
 
   public static void main(String... args) {
     try (Scanner scan = new Scanner(System.in)) {
       int dataCount = Integer.parseInt(scan.nextLine(), 10);
-      for (int i = 0; i < dataCount; i++, caseNum++)
+      for (int i = 0; i < dataCount; i++)
         each(scan);
     }
   }
