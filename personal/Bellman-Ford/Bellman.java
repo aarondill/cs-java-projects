f941afb3028966bf71051abb01b1b41101c7d557
Aaron Dill <aaronsacks2006@gmail.com>
2025-03-31 12:45:56 -0500

    feat: bellman detect negative cycle

--- personal/Bellman-Ford/Bellman.java
+++ personal/Bellman-Ford/Bellman.java
@@ -8,25 +8,31 @@ import java.util.stream.Collectors;
 public class Bellman {
   public final static record Result<T>(Map<Node<T>, Integer> distances, Map<Node<T>, Node<T>> prev) {}
 
+  /**
+   * Returns the shortest path from start to every node in the graph. The result
+   * is a map of nodes to their distances from start. This is slower than
+   * Dijkstra's algorithm, but it works for negative weights!
+   */
   public static <T> Result<T> bellmanFord(Node<T> start) {
-    // Initialize
     Map<Node<T>, Integer> distances = new HashMap<>();
     Map<Node<T>, Node<T>> prev = new HashMap<>();
     distances.put(start, 0);
     // Keep relaxing edges until we have no more new edges (i=0, size=1; i=1, size>1; i=V-1, size=V)
-    for (int i = 0; i < distances.size(); i++) {
-      boolean changed = false;
-      for (var from : new HashSet<>(distances.keySet())) { // seen nodes
+    // Run until <= size to detect negative cycles
+    for (int i = 0; i <= distances.size(); i++) {
+      HashSet<Node<T>> copy = new HashSet<>(distances.keySet()); // copy to avoid concurrent modification
+      for (var from : copy) { // seen nodes
         for (Node<T> to : from.edges.keySet()) {
           int newDistance = distances.get(from) + from.edges.get(to);
           if (newDistance < distances.getOrDefault(to, Integer.MAX_VALUE)) {
+            // If we find a shorter path after considering all edges, there is a negative cycle
+            if (i == distances.size()) throw new IllegalStateException("Negative cycle detected");
             distances.put(to, newDistance);
             prev.put(to, from);
-            changed = true;
           }
         }
       }
-      if (!changed) break; // we're done!
+      if (copy.equals(distances.keySet())) break; // no changes, we're done!
     }
     return new Result<>(distances, prev);
   }
@@ -41,21 +47,22 @@ public class Bellman {
     var f = new Node<>("F");
 
     // Connect nodes
-    a.addEdge(b, 10).addEdge(c, 15);
-    b.addEdge(d, 12).addEdge(f, 15);
-    d.addEdge(f, 1).addEdge(e, 2);
-    f.addEdge(e, 1);
-    c.addEdge(e, -10);
+    a.addEdge(c, 1);
+    c.addEdge(e, -2);
+    e.addEdge(a, 1);
+    a.addEdge(b, 5);
+    b.addEdge(d, 2);
 
     // Create graph
     var start = a;
-    var end = e;
+    var end = d;
     var result = bellmanFord(start);
 
     // Retreive shortest path from end to start
     List<Node<String>> path = new ArrayList<>();
-    for (var curr = end; curr != null; curr = result.prev().get(curr))
+    for (var curr = end; curr != start; curr = result.prev().get(curr))
       path.add(curr);
+    path.add(start);
 
     // Print the result
     System.out.printf("Shortest distance from %s to %s:\n", start, end);
