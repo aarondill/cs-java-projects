7963c8a05bb821b7208a5be4df1d5eee3f5ccc15
Aaron Dill <aaronsacks2006@gmail.com>
2025-04-07 07:59:41 -0500

    feat(template): add version

--- template/app/src/main/java/{{ $ROOT }}.java
+++ template/app/src/main/java/{{ $ROOT }}.java
@@ -1,4 +1,19 @@
 public class {{ $ROOT }} {
+  final static public String VERSION = version();
+
+  /** Get the program version on startup. */
+  private static String version() {
+    Properties p = new Properties();
+    try (Reader r =
+        new InputStreamReader(MethodHandles.lookup().lookupClass().getResourceAsStream("/version.properties"))) {
+      p.load(r);
+    } catch (IOException e) {
+      System.err.println("Error reading version.properties");
+      e.printStackTrace();
+    }
+    return p.getProperty("version");
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
--- template/app/src/test/java/{{ $ROOT }}Test.java
+++ template/app/src/test/java/{{ $ROOT }}Test.java
@@ -3,6 +3,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class {{ $ROOT }}Test {
   @Test
+  void version() throws IOException {
+    Properties properties = new Properties();
+    try(Reader gradelProperties = new InputStreamReader(getClass().getResourceAsStream("/version.properties"))){
+      properties.load(gradelProperties);
+    }
+    assertTrue(properties.getProperty("version").equals({{ $ROOT }}.VERSION));
+  }
+  @Test
   void test() {
     System.out.println("NOTE: There is no test for this app yet!");
     assertTrue(true);
