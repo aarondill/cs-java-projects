a6065ec3a3eff06d572ad0948c4c6011a6727193
Aaron Dill <aaronsacks2006@gmail.com>
2025-04-08 00:03:55 -0500

    fix: no more timeout on dean

--- UIL/2024-2025/district/Dean/Dean.java
+++ UIL/2024-2025/district/Dean/Dean.java
@@ -1,31 +1,66 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Dean {
   @SuppressWarnings("unused")
-  private static int caseNum = 1;
+  private static long caseNum = 1;
   private static final String INPUT_FILE = "dean.dat";
 
-  private static void each(Scanner scan) {
-    // Parse the input:
-    String s = scan.nextLine();
-    int num = 0;
-    for (int i = 1; i <= s.length(); i++) {
-      for (int j = 0; j <= s.length() - i; j++) {
-        String sub = s.substring(j, j + i);
-        if (new StringBuilder(sub).reverse().toString().equals(sub)) {
-          num++;
-        }
-      }
+  // Takes a window [i, j) and expands it [i-1, j+1), [i-2, j+2), etc, until it stops finding palindromes
+  // Assumes the window passed in is a palindrome
+  public static long countExpandingPalindromes(List<Character> s, int i, int j) {
+    long count = 0;
+    // we're *still* a palindrome iff the new first and last characters are the same
+    // PERF: This is the most important optimization! If you try to expand the
+    // window, then test if it's a palindrome, it *will* timeout. This is
+    // because we're checking the entire string every time.
+    while (s.get(i).equals(s.get(j - 1))) {
+      count++;
+      if (--i < 0 || ++j > s.size()) break;
     }
-    System.out.println(num);
+    return count;
+  }
+
+  /**
+   * Counts the number of palindromes in the given string. Loops each window of
+   * len 2 and 3, then if it finds a palindrome, expands the window until it's no
+   * longer a palindrome.
+   *
+   * @param s the string to count palindromes in
+   * @param j the index of the last character in the window (exclusive)
+   * @return the number of palindromes in the given string
+   */
+  public static long countPalindromes(List<Character> s, int len) {
+    long count = 0;
+    for (int idx = 0; idx + len <= s.size(); idx++) { // for each window of length len
+      List<Character> win = s.subList(idx, idx + len); // window from idx of length len
+      if (!win.equals(win.reversed())) continue;
+      count += countExpandingPalindromes(s, idx, idx + len); // expand the window as long as we can, this counts the original window;
+    }
+    return count;
+  }
+
+  public static long countPalindromes(String str) {
+    // This is more efficient because we can use subList instead of copying the substring
+    List<Character> s = str.chars().mapToObj(c -> (char) c).toList();
+    return countPalindromes(s, 2) + countPalindromes(s, 3);
+  }
+
+  private static void each(Scanner scan) {
+    String s = scan.nextLine();
+    long count = s.length() + countPalindromes(s);// One letter palindromes count
+    System.out.println(count);
   }
 
   public static void main(String... args) throws FileNotFoundException {
     try (Scanner scan = new Scanner(new File(INPUT_FILE))) {
-      int dataCount = Integer.parseInt(scan.nextLine(), 10);
-      for (int i = 0; i < dataCount; i++, caseNum++)
+      long dataCount = Integer.parseInt(scan.nextLine(), 10);
+      for (long i = 0; i < dataCount; i++, caseNum++)
         each(scan);
     } catch (FileNotFoundException e) {
       System.err.println("Could not find file: " + INPUT_FILE);
